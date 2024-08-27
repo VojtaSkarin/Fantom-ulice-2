09fcbecbc198e107936adf324f31c3381e69e7fd
Vojta Škaroupka <vojtaskaroupka@seznam.cz>
2024-08-27 12:16:24 +0200

    27. 8. 2024

--- play.php
+++ play.php
@@ -11,23 +11,29 @@ session_start();
 
 // Constants
 // Mode of stat change
-define("INITIALIZE", 1);
-define("SET", 2);
-define("CHANGE", 3);
+enum Mode: int {
+	case Initialize = 1;
+	case Set = 2;
+	case Change = 3;
+}
 
 // Stats
-define("COMBAT_SKILL", 1);
-define("STAMINA", 2);
-define("LUCK", 1);
-define("FIREPOWER", 3);
-define("ARMOUR", 4);
-define("ROCKETS", 5);
-define("NAILS", 6);
-define("OIL", 7);
+define('COMBAT_SKILL', 'Combat_Skill');
+define('STAMINA', 'Stamina');
+define('LUCK', 'Luck');
+define('MED_KIT', 'Med_Kit');
+define('CREDITS', 'Credits');
+define('FIREPOWER', 'Firepower');
+define('ARMOUR', 'Armour');
+define('ROCKETS', 'Rockets');
+define('NAILS', 'Nails');
+define('OIL', 'Oil');
+
+define('STATS', array(COMBAT_SKILL, STAMINA, LUCK, MED_KIT, CREDITS, FIREPOWER, ARMOUR, ROCKETS, NAILS, OIL));
 
 // Type of stat
-define("MAX", 1);
-define("ACT", 2);
+define('MAX', 'maximum');
+define('ACT', 'actual');
 
 // Others
 define("ITEMS", array(
@@ -77,21 +83,18 @@ if (array_key_exists('action', $_GET)) {
 // Modify stats
 if (! $_SESSION['executed'] || true) {
 	$_SESSION['executed'] = true;
-	
-	define("INIT_VALUES", array(
-		[1, 6], // Combat skill
-		[2, 24], // Stamina
-		[1, 6], // Luck
-		[1, 6], // Firepower
-		[2, 24], // Armour
-		[0, 4], // Rockets
-		[0, 3], // Nails
-		[0, 2], // Oil
-	));
 
-	for ($i = 0; $i < count($data->stats); $i++) {
-		update_stat($_SESSION['stats'][$i], $data->stats[$i], INIT_VALUES[$i]);
-	}
+	// Stats
+	update_stat($_SESSION['stats'][COMBAT_SKILL], $data->stats->combat_skill, 1, 6);
+	update_stat($_SESSION['stats'][STAMINA], $data->stats->stamina, 2, 24);
+	update_stat($_SESSION['stats'][LUCK], $data->stats->luck, 1, 6);
+	update_stat($_SESSION['stats'][MED_KIT], $data->stats->med_kit);
+	update_stat($_SESSION['stats'][CREDITS], $data->stats->credits);
+	update_stat($_SESSION['stats'][FIREPOWER], $data->stats->firepower, 1, 6);
+	update_stat($_SESSION['stats'][ARMOUR], $data->stats->armour, 2, 24);
+	update_stat($_SESSION['stats'][ROCKETS], $data->stats->rockets);
+	update_stat($_SESSION['stats'][NAILS], $data->stats->nails);
+	update_stat($_SESSION['stats'][OIL], $data->stats->oil);
 }
 
 /* Render page*/
@@ -114,7 +117,7 @@ foreach ($data->title_middle as $title) {
 foreach ($data->story as $paragraph) {
 	echo "<div class=\"text\">\n";
 	echo $paragraph."\n";
-	echo "</div>\n";
+	echo "</div>\n\n";
 }
 
 // Image
@@ -129,34 +132,172 @@ foreach($data->options as $i => $option) {
 	echo "</div>\n\n";
 }
 
+// Note
+foreach ($data->notes as $note) {
+	echo "<div class=\"note\">\n";
+	echo $note."\n";
+	echo "</div>\n\n";
+}
+
 // HUD
-echo $data->show_hud;
 if ($data->show_hud) {
-	echo "<div class=\"stats\">\n";
+	print_r($_SESSION['stats']);
+	
+	echo "<div class=\"stat-block\">\n";
 	
 	// Combat skill
-	for ($i = 0; $i < count($_SESSION['stats']); $i++) {
-		echo 'UMĚNÍ BOJE: ' . $_SESSION['stats'][COMBAT_SKILL][ACT] . '/' . $umeni_boje_max . "<br>\n";
+	echo "<div class=\"stat\">\n";
+	$csm = $_SESSION['stats'][COMBAT_SKILL][MAX];
+	$csa = $_SESSION['stats'][COMBAT_SKILL][ACT];
+	echo 'UMĚNÍ BOJE: ' . $csm . '/' . $csa . "\n";
+	echo "</div>\n\n";
+
+	// Stamina
+	echo "<div class=\"stat\">\n";
+	$sm = $_SESSION['stats'][STAMINA][MAX];
+	$sa = $_SESSION['stats'][STAMINA][ACT];
+	echo 'STAMINA: ' . $sa . '/' . $sm . "\n";
+	echo "</div>\n\n";
+
+	// Luck
+	echo "<div class=\"stat\">\n";
+	$lm = $_SESSION['stats'][LUCK][MAX];
+	$la = $_SESSION['stats'][LUCK][MAX];
+	echo 'ŠTĚSTÍ: ' . $la . '/' . $lm . "\n";
+	echo "</div>\n";
+	
+	echo "</div>\n\n";
+
+	echo "<div class=\"stat-block\">\n";
+
+	// MedKit
+	echo "<div class=\"stat\">\n";
+	echo 'MED-KIT: ';
+	$medkit = $_SESSION['stats'][MED_KIT][ACT];
+	for ($i = 0; $i < $medkit; $i++) {
+		echo "<img class=\"medkit\" src=\"images/medkit.png\">";
+	}
+	echo "\n</div>\n\n";
+
+	// Credits
+	echo "<div class=\"stat\">\n";
+	$credits = $_SESSION['stats'][CREDITS][ACT];
+	echo 'KREDITY: ' . $credits;
+	echo "<img class=\"credit\" src=\"images/credit.png\">\n";
+	echo "</div>\n";
+	
+	echo "</div>\n\n";
+
+	echo "<div class=\"stat-block\">\n";
+	
+	// Firepower
+	echo "<div class=\"stat\">\n";
+	$fpm = $_SESSION['stats'][FIREPOWER][MAX];
+	$fpa = $_SESSION['stats'][FIREPOWER][ACT];
+	echo 'PALEBNÁ SÍLA: ' . $fpa . '/' . $fpm . "\n";
+	echo "</div>\n\n";
+
+	// Armour
+	echo "<div class=\"stat\">\n";
+	$am = $_SESSION['stats'][ARMOUR][MAX];
+	$aa = $_SESSION['stats'][ARMOUR][ACT];
+	echo 'PANCÍŘ: ' . $aa . '/' . $am . "\n";
+	echo "</div>\n";
+	
+	echo "</div>\n\n";
+	
+	echo "<div class=\"stat-block\">\n";
+
+	// Rockets
+	echo "<div class=\"stat\">\n";
+	$rockets = $_SESSION['stats'][ROCKETS][ACT];
+	echo 'RAKETY: ';
+	for ($i = 0; $i < $rockets; $i++) {
+		echo '<img class="rocket" src="images/rocket.png">';
+	}
+	echo "\n</div>\n\n";
+
+	// Nails
+	echo "<div class=\"stat\">\n";
+	$nails = $_SESSION['stats'][NAILS][ACT];
+	echo 'ŽELEZNÉ BODCE: ';
+	for ($i = 0; $i < $nails; $i++) {
+		echo '<img class="nails" src="images/nails.png">';
+	}
+
+	echo "\n</div>\n\n";
+
+	// Oil
+	echo "<div class=\"stat\">\n";
+	$oil = $_SESSION['stats'][OIL][ACT];
+	echo 'OLEJ: ';
+	for ($i = 0; $i < $oil; $i++) {
+		echo '<img class="oil" src="images/oil.png">';
+	}
+
+	echo "\n</div>\n\n";
+
+	// Wheels
+$kola = $_SESSION['kola'];
+
+echo 'REZERVNÍ KOLA: ';
+
+for ($i = 0; $i < $kola; $i++) {
+	echo '<img class="wheel" src="images/wheel.png">';
+}
+
+echo "<br>\n";
+
+$palivo = $_SESSION['palivo'];
+
+echo 'PALIVO: ';
+
+for ($i = 0; $i < $palivo; $i++) {
+	echo '<img class="canister" src="images/canister.png">';
+}
+
+echo "<br><br>\n";
+
+echo 'VYBAVENÍ: ';
+
+$vybaveni = $_SESSION['vybaveni'];
+
+$i = 0;
+foreach ($vybaveni as $predmet) {
+	if ($predmet[0] == '_') {
+		continue;
 	}
 	
-	echo "</div>\n";
+	if ($i > 0) {
+		echo ', ';
+	}
+	
+	echo $predmet;
+	$i++;
+}
+
+if (count($vybaveni) == 0) {
+	echo 'žádné';
+}
+	
+	echo "</div>\n\n";
 }
 
 // Helper functions
-function update_stat(&$stat, $data, $init_values) {
-	if ($data->mode == INITIALIZE) {
+function update_stat(&$stat, $data, $throws = -1, $shift = -1) {
+	if ($data->mode == Mode::Initialize->value) {
 		$value = 0;
-		for ($i = 0; $i < $init_values[0]; $i++) {
+		for ($i = 0; $i < $throws; $i++) {
 			$value += rand(1, 6);
 		}
-		$stat[ACT] = $stat[MAX] = $value + $init_values[1];
-	} else if ($data->mode == SET) {
-		$stat[$ACT] = $data->value;
-	} else if ($data->mode == CHANGE) {
+		$stat[ACT] = $stat[MAX] = $value + $shift;
+	} else if ($data->mode == Mode::Set->value) {
+		$stat[ACT] = $data->value;
+	} else if ($data->mode == Mode::Change->value) {
 		if ($data->value > 0) {
-			$stat[$ACT] = min($stat[$MAX], $stat[$ACT] + $data->value);
+			$stat[ACT] = min($stat[MAX], $stat[ACT] + $data->value);
 		} else {
-			$stat[$ACT] = max(0, $stat[$ACT] + $data->value);
+			$stat[ACT] = max(0, $stat[ACT] + $data->value);
 		}
 	}
 }
---
+++ update.php
@@ -0,0 +1,71 @@
+<!-- Remove from release -->
+
+<?php
+$path = 'C:\xampp\htdocs\Fantom-ulice-2\content';
+
+foreach (scandir($path) as $file) {
+	if (pathinfo($file, PATHINFO_EXTENSION) == 'json') {
+		$filename = $path . '\\' . $file;
+		
+		// Decode
+		$file_content = file_get_contents($filename);
+		$sanitized = str_replace(["\r\n", "\t"], '', $file_content);
+		try {
+			$data = json_decode($sanitized, null, 512, JSON_THROW_ON_ERROR);
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+		
+		// Update
+		add_content($data);
+		
+		// Encode
+		$encoded = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+		echo $encoded;
+		file_put_contents($filename, $encoded);
+	}
+}
+
+function add_content($object) {
+	// Note
+	if (! property_exists($object, 'notes')) {
+		$object->notes = array();
+	}
+	
+	// Show HUD
+	if (! property_exists($object, 'show_hud')) {
+		$object->show_hud = false;
+	}
+	
+	// Stats
+	$stat = new stdClass();
+	$stat->mode = 3; /* CHANGE */
+	$stat->value = 0;
+	
+	$stats = new stdClass();
+	$stats->combat_skill = $stat;
+	$stats->stamina = $stat;
+	$stats->luck = $stat;
+	$stats->med_kit = $stat;
+	$stats->credits = $stat;
+	$stats->firepower = $stat;
+	$stats->armour = $stat;
+	$stats->rockets = $stat;
+	$stats->nails = $stat;
+	$stats->oil = $stat;
+	$stats->wheels = $stat;
+	$stats->fuel = $stat;
+	
+	if (! property_exists($object, 'stats')) {
+		$object->stats = $stats;
+	}
+	
+	if (! property_exists($object->stats, 'wheels')) {
+		$object->stats->wheels = $stat;
+	}
+	
+	if (! property_exists($object->stats, 'fuel')) {
+		$object->stats->fuel = $stat;
+	}
+}
+?>
